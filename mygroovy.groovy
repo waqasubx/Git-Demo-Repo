75b162f93bab806ba97f8c855553861a5e8f9959
waqasubx <100279574+waqasubx@users.noreply.github.com>
2022-11-30 15:30:32 +0500

    Update mygroovy.groovy

--- mygroovy.groovy
+++ mygroovy.groovy
@@ -1,12 +1,21 @@
 #!groovy
 
-pipeline {
-    agent any
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World from mygroovy.groovy'
-            }
-        }
-    }
+// pipeline {
+//     agent any
+//     stages {
+//         stage('Hello') {
+//             steps {
+//                 echo 'Hello World from mygroovy.groovy'
+//             }
+//         }
+//     }
+// }
+def example1() {
+  println 'Hello from example1'
 }
+
+def example2() {
+  println 'Hello from example2'
+}
+
+return this
