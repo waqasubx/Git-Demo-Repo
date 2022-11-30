908b57bfd9f507f43c9e3b7d66770ff5c343efba
waqasubx <100279574+waqasubx@users.noreply.github.com>
2022-11-30 15:00:27 +0500

    Update mygroovy.groovy

--- mygroovy.groovy
+++ mygroovy.groovy
@@ -3,6 +3,10 @@
 pipeline {
     agent any
     stages {
+        stage('Checkout')
+        {
+            GitManager.clone(this, "https://github.com/waqasubx/Git-Demo-Repo.git", "*/main", "waqasubx");
+        }
         stage('Hello') {
             steps {
                 echo 'Hello World'
